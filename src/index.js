9ff9b81f00921bba54221a1be1dcc2168fb60cea
asus <aaa@aaa.aaa>
2020-10-23 14:24:27 +0500

    feat: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+  if(n<0){
+      n*=-1;
+  }
+  var splitString = n.toString().split(""); 
+
+ 
+  var reverseArray = splitString.reverse();
+
+
   
+  var joinArray = reverseArray.join(""); 
+
+
+  return joinArray; 
 }
